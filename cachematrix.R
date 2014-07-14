8176c16b8ab7b61f427580a759edf1d7b42f619b
Axji <axel.zenklusen@gmail.com>
2014-07-14 16:22:25 +0200

    Implementation 1
    to be commented

--- cachematrix.R
+++ cachematrix.R
@@ -3,11 +3,30 @@
 
 # Makes a Cache Matrix
 makeCacheMatrix <- function(x = matrix()) {
-	x <- rbind(c(1, -1/4), c(-1/4, 1))
+	invert <- NULL
+	
+	set <- 	function(y) {
+                x <<- y
+                
+			}
+	get <- function() x
+	
+	setinv <-function(inverse) invert <<-inverse
+	getinv <- function() invert
+	
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 # inverts the Matrix
 cacheSolve <- function(x) {
-        return solve(x)%*%x
+        invert <- x$getinv()
+		if(!is.null(invert){
+			return(invert)
+		}
+		mx <- x$get()
+		invert = solve(mx)%*%mx
+		x$setinv(invert)
+		return invert
+		
 }
