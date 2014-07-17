2ff249112bd301408cd6cc51d4a39bd8ffd814a1
Axji <axel.zenklusen@gmail.com>
2014-07-17 16:49:32 +0200

    Revert "Revert "Micro Correction & Comments""
    This reverts commit d66a03b5b7eddd06f9af786f585482386695dcbe.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-# Makes a Cache Matrix
+# Makes a Matrix so it can cache the inverted Matrix of it self
 makeCacheMatrix <- function(x = matrix()) {
 	invert <- NULL
 	
 	set <- 	function(y) {
                 x <<- y
-                
-			}
+            }
 	get <- function() x
 	
 	setinv <-function(inverse) invert <<-inverse
@@ -18,7 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-# inverts the Matrix
+# inverts the Matrix if its already inverted take the one from the Cache
 cacheSolve <- function(x) {
         invert <- x$getinv()
 		if(!is.null(invert){
